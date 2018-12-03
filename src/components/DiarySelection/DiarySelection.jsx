4c3a804d93efb8f367b4ddb64bd27bfeb4c4ab85
Kirill Shestakov <>
2018-12-03 04:53:48 -0800

    Some simple idea

--- src/components/DiarySelection/DiarySelection.jsx
+++ src/components/DiarySelection/DiarySelection.jsx
@@ -5,7 +5,33 @@ import { WriteSecurelyLabel } from "../WriteSecurelyLogo/WriteSecurelyLogo";
 export function DiarySelection() {
     return (
         <div class="DiarySelection">
-            Diary Selection
+            <div class="DiarySelection__Content">
+                <DiaryBook
+                    title="+"
+                />
+                <DiaryBook
+                    title="Personal diary"
+                />
+                <DiaryBook
+                    title="Poetry book"
+                />
+                <DiaryBook
+                    title="Diary for ideas"
+                />
+                <DiaryBook
+                    title="Secret wishes"
+                />
+            </div>
         </div>
     );
+}
+
+export function DiaryBook({ title }) {
+    return (
+        <div class="DiaryBook">
+            <div class="DiaryBook__Title">
+                { title }
+            </div>
+        </div>
+    )
 }

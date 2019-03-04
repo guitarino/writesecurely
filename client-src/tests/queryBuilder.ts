805c1a846f19e16b8e1dd92e941845b7e2beaa4b
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2019-03-04 05:33:21 -0800

    a first unit test... still work to do with auto path detection

---
+++ client-src/tests/queryBuilder.ts
@@ -0,0 +1,12 @@
+import "../modules/QueryBuilder/QueryBuilder";
+import { QueryBuilder } from "../modules/QueryBuilder/QueryBuilder.types";
+import { get } from "../type/inject";
+import assert from 'assert';
+
+const queryBuilder: QueryBuilder = get<QueryBuilder>(QueryBuilder);
+
+describe('query builder', () => {
+    it('is object', () => {
+        assert(typeof queryBuilder === "object");      
+    });
+});

73853f45bf8261c4dbcb730cbd5610557d986443
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2019-04-14 01:32:50 -0700

    removing old preact component dependency connection

--- client-src/type/inject.ts
+++ client-src/type/inject.ts
@@ -12,14 +12,3 @@ export const {
     configure
 } = container;
 export const { dependency, inject } = createDecorators(container);
-
-type getComponentFromType<T> =
-    T extends TypeIdentifier<infer U> ? U :
-    T extends MultiTypeIdentifier<infer U> ? U :
-    T extends LazyTypeIdentifier<infer U> ? U :
-    T extends LazyMultiTypeIdentifier<infer U> ? U :
-    never;
-
-export function getComponentImplementation<T>(ComponentType: T): getComponentFromType<T> {
-    return getImplementation(ComponentType as any) as any;
-}

d27b4867a5e030709e2807cd550bedfcfc563545
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2019-04-28 16:46:00 -0700

    changing the background a bit, and adding transition

--- client-src/ui/components/Paper/Paper.tsx
+++ client-src/ui/components/Paper/Paper.tsx
@@ -4,19 +4,21 @@ import "./Paper.scss";
 
 export type PaperProps = {
     className?: string,
+    style?: JSX.HTMLAttributes['style'],
     children?: ComponentChildren
 };
 
 export class Paper extends Component<PaperProps> {
     static defaultProps: OptionalOf<PaperProps> = {
+        style: {},
         className: '',
         children: null
     }
 
     render() {
-        const { children, className } = this.props;
+        const { children, className, style } = this.props;
         return (
-            <div className={`Paper ${className}`}>
+            <div className={`Paper ${className}`} style={style}>
                 {children}
             </div>
         );
